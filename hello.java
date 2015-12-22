b2d496b666fbd911bec0bdf251756c5e2ca8ab01
dlgudrms89 <dlgudrms8989@gmail.com>
2015-12-22 09:39:32 +0900

    added output "Bitkeeper"

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 		System.out.println("What is Git?");
+
+		System.out.println("2002 gitkeeper");
 	}
 }	
