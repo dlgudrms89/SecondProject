4d5dc6e3bbd24c0c980f9f7fee2999a23a589a7a
dlgudrms89 <dlgudrms8989@gmail.com>
2015-12-29 09:27:03 +0900

    github for git pull

--- hello.java
+++ hello.java
@@ -1,15 +1,13 @@
 //for command line git tutorial
 //local repository modification
 //github modification
-public class hello {
+public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 		System.out.println("What is Git?");
-
-
 		System.out.println("That's DVCS");
-
 		System.out.println("2002 gitkeeper");
+		System.out.println("That's version control");
 
 	}
 }	
